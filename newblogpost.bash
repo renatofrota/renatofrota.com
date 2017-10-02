1ae8741e7acad9dba55349e69f4ed381e4745021
Renato Frota <renatofrota@gmail.com>
2017-10-02 07:44:28 -0300

    new script to create blog posts

---
+++ newblogpost.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+date=$(date +%Y-%m-%d)
+time=$(date +%H:%M:%S)
+#IFS=
+read -erp "Nome da nova publicação: " postname
+slug="$(echo $postname | iconv -f utf-8 -t ascii//translit | sed 's, ,-,g' | tr '[:upper:]' '[:lower:]' | sed -e 's,[^a-z0-9\._-],,g')"
+file="content/blog/${date}-$slug.md"
+[[ -d "$file" ]] && echo "File exists: $file" && exit 1
+echo "---
+date: \"$date $time\"
+title: \"$postname\"
+slug: \"$slug\"
+description: \"\"
+---
+" > $file
+vim $file
+exit 0
